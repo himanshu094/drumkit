9f03d2531ac3a7b8b7b4cb7761954a3772699097
himanshu shakya <116728762+himanshu094@users.noreply.github.com>
2023-03-27 16:17:33 +0530

    buttton style remain

---
+++ Drum Kit/index.js
@@ -0,0 +1,70 @@
+'use strict'
+var numb=document.querySelectorAll(".drum").length
+
+for(var i=0;i<numb;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+
+         this.style.color="white"
+         var btnOn=this.innerHTML
+         makeSound(btnOn)
+   })
+}
+
+// document.addEventListener("keypress",function(event){
+//     alert("key pressed")
+// })
+
+document.addEventListener("keypress",function(press){
+  
+    makeSound(press.key)
+})
+
+function makeSound(key){
+       
+    switch (key){
+        case "w":
+        var t1 = new Audio("sounds/tom-1.mp3")
+        t1.play()
+        break;
+
+        case "a":
+        var t2 = new Audio("sounds/tom-2.mp3")
+        t2.play()
+        break;
+
+        case "s":
+        var t3 = new Audio("sounds/tom-3.mp3")
+        t3.play()
+        break;
+
+        case "d":
+        var t4 = new Audio("sounds/tom-4.mp3")
+        t4.play()
+        break;
+
+        case "j":
+        var t5 = new Audio("sounds/crash.mp3")
+        t5.play()
+        break;
+
+        case "k":
+        var t6 = new Audio("sounds/kick-bass.mp3")
+        t6.play()
+        break;
+
+        case "l":
+        var t7 = new Audio("sounds/snare.mp3")
+        t7.play()
+        break;
+    }
+
+}
+// function handleClick(){
+
+//     alert("hii")
+// }
+
+// var daudio=new Audio("sounds/tom-1.mp3");
+// daudio.play();
+// 
